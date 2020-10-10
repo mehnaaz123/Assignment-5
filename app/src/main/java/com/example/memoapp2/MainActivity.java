b2d30544d70a83490f90d546ef2fe18b315b372e
mehnaaz123 <m246rahm@ryerson.ca>
2020-10-10 17:24:45 -0400

    4

--- app/src/main/java/com/example/memoapp2/MainActivity.java
+++ app/src/main/java/com/example/memoapp2/MainActivity.java
@@ -3,24 +3,32 @@ package com.example.memoapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    //Note: still have to add audio!!
+    MediaPlayer song = new MediaPlayer();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        song = MediaPlayer.create(this, R.raw.ukulele);
+        song.start();
+
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
+                song.pause();
                 finish();
                 startActivity(new Intent(MainActivity.this, ItemListActivity.class));
             }
         };
+
         Timer opening = new Timer();
         opening.schedule(task, 5000);
     }
